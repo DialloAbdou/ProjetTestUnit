e53683a475a3602e91fdf077269989e33b0aea9e
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-04-04 16:27:06 +0200

    factorisation

--- Chapitre_02/FR_1915_02_02/FR_1915_ListeTachesTests/Tachetest.cs
+++ Chapitre_02/FR_1915_02_02/FR_1915_ListeTachesTests/Tachetest.cs
@@ -7,16 +7,33 @@ namespace FR_1915_ListeTachesTests
     [TestClass]
     public class Tachetest
     {
-        private readonly TimeSpan _3j_ = TimeSpan.FromDays(3);
-        private readonly TimeSpan _2j_ = TimeSpan.FromDays(2);
-        private readonly TimeSpan _1J_ = TimeSpan.FromDays(1);
-        private readonly TimeSpan _0J_ = TimeSpan.FromDays(0);
+        private readonly TimeSpan
+            _3j_ = TimeSpan.FromDays(3),
+            _2j_ = TimeSpan.FromDays(2),
+           _1J_ = TimeSpan.FromDays(1),
+           _0J_ = TimeSpan.FromDays(0);
+        private Tache tacheAvril;
 
-        [TestMethod]
-        public void Initialiser_3JourAudebutAvril_DefinirTitre_ResterAfaire_NonTerminer()
+        [TestInitialize]
+        public void InitTest()
         {
             DateTime deb1avril3j = new DateTime(2014, 4, 1);
             Tache tacheAvril = new Tache("Poisson Avril", deb1avril3j, _3j_);
+        }
+
+        private Tache NouvelleTache1erAvril(string titre ="Poisson Avril", int duree = 3)
+        {
+            return new Tache(titre, new DateTime(2014, 4, 1), _3j_);
+        }
+
+        //private Tache NouvelleTacheAvrilEntame(int duree, int fait)
+        //{
+        //    var tache = NouvelleTache1erAvril(, duree);
+        //}
+        [TestMethod]
+        public void Initialiser_3JourAudebutAvril_DefinirTitre_ResterAfaire_NonTerminer()
+        {
+            tacheAvril = NouvelleTache1erAvril("Poisson Avril", 3);
             Assert.AreEqual("Poisson Avril", tacheAvril.Titre);
             Assert.AreEqual(_3j_, tacheAvril.Duree);
             Assert.AreEqual(_3j_, tacheAvril.ResteAFaire);
@@ -25,8 +42,7 @@ namespace FR_1915_ListeTachesTests
         [TestMethod]
         public void effectuer_2JTraivailResteAfaire_NonTerminer()
         {
-            DateTime deb1avril3j = new DateTime(2014, 4, 1);
-            Tache tacheAvril = new Tache("Poisson Avril", deb1avril3j,_3j_);
+            tacheAvril = NouvelleTache1erAvril("Poisson Avril", 3);
             Assert.AreEqual("Poisson Avril", tacheAvril.Titre);
             tacheAvril.Effectuer(_2j_);
             Assert.AreEqual(_1J_, tacheAvril.ResteAFaire);
